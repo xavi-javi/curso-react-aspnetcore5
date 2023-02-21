fecae5256d5bd4c4baa90e7154187efd160ae9d4
Javier Saucedo Rodriguez <xavi.javier.sau@gmail.com>
2023-02-21 15:07:30 -0600

    36. Expresiones en JSX

--- peliculas-app/src/App.tsx
+++ peliculas-app/src/App.tsx
@@ -3,10 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const subtitulo = "Este es un subtitulo";
+
+  const duplicar = ( valor: number ) => valor * 2;
+
+  const imagenURL = "https://blog.digital-pineapple.com.mx/wp-content/uploads/2021/01/0_oZLL-N4dGNlBe4Oh.png";
+
   return (
     <div>
       <h1>Hola Mundo!</h1>
-      <h3>Este es un subtitulo</h3>
+      <h3>{subtitulo.toUpperCase()}</h3>
+      <h4>El doble de 3 es {duplicar(3)}</h4>
+      <img alt="Logo React" src={imagenURL}></img>
+      <div>
+        <input type="checkbox" checked={true}/> Este es un checkbox
+      </div>
     </div>
   );
 }
