170eacd5427b02afe8d4fe3eb9ac4b9edc202c6e
Javier Saucedo Rodriguez <xavi.javier.sau@gmail.com>
2023-02-21 14:24:11 -0600

    32. React vs HTML - El DOM Virtual en Acción

--- peliculas-app/src/App.tsx
+++ peliculas-app/src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [fecha, setFecha] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setFecha(new Date());
+    }, 1000);
+
+    return () => clearInterval(timerId);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Ejemplo REACT</h3>
+      <input />      
+      <div>{fecha.toString()}</div>
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
