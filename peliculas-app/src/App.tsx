013e9328168286464b33c19c1af48f4b221541ea
Javier Saucedo Rodriguez <xavi.javier.sau@gmail.com>
2023-02-21 14:52:44 -0600

    35. Introducción a JSX

--- peliculas-app/src/App.tsx
+++ peliculas-app/src/App.tsx
@@ -3,40 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const [fecha, setFecha] = useState(new Date());
-
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      setFecha(new Date());
-    }, 1000);
-
-    return () => clearInterval(timerId);
-  });
-
   return (
     <div>
-      <h3>Ejemplo REACT</h3>
-      <input />      
-      <div>{fecha.toString()}</div>
+      <h1>Hola Mundo!</h1>
+      <h3>Este es un subtitulo</h3>
     </div>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
