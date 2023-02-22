c131929a39a6e3c24a93fe322fe18f699b639b70
Javier Saucedo Rodriguez <xavi.javier.sau@gmail.com>
2023-02-21 20:11:11 -0600

    38. Eventos

--- peliculas-app/src/App.tsx
+++ peliculas-app/src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import AppCSS from "./App.module.css";
 
 function App() {
+  let texto = "";
+
   const subtitulo = "Este es un subtitulo";
 
   const duplicar = (valor: number) => valor * 2;
@@ -11,30 +12,25 @@ function App() {
   const imagenURL =
     "https://coder.clothing/images/stories/virtuemart/product/reactjs-logo-sticker.jpg";
 
-  const cuadradoAzul = {
-    backgroundColor: "blue",
-    width: "50px",
-    height: "50px",
-    marginLeft: "1rem",
-  };
+    const manejarClick = () => {
+      console.log('click');
+    };
+
+    const manejarKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      console.log(e.currentTarget.value);
+      texto = e.currentTarget.value;
+    };
 
   return (
     <>
-      <div className="cuadrado-rojo"></div>
-      <div
-        style={{
-          backgroundColor: "green",
-          width: "50px",
-          height: "50px",
-          marginLeft: "1rem",
-        }}
-      ></div>
-      <div style={cuadradoAzul}></div>
       <h1>Hola Mundo!</h1>
+      <button onClick={manejarClick}>Click evento</button>      
+      <input type="text" onKeyUp={manejarKeyUp} />
+      <div>
+        El texto ingresado es: {texto}
+      </div>
       <h3>{subtitulo.toUpperCase()}</h3>
       <h4>El doble de 3 es {duplicar(3)}</h4>
-      <h4 className="color">Color desde index.css</h4>
-      <h4 className={AppCSS.color}>Color desde App.module.css</h4>
       <img alt="Logo React" src={imagenURL}></img>
       <div>
         <input type="checkbox" checked={true} /> Este es un checkbox
