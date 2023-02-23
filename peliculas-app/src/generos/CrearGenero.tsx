c76bdbbbb5d88298e8adc77f9a17a849364c6639
Javier Saucedo Rodriguez <xavi.javier.sau@gmail.com>
2023-02-22 21:11:43 -0600

    66. Navegando con JavaScript y useHistory

--- peliculas-app/src/generos/CrearGenero.tsx
+++ peliculas-app/src/generos/CrearGenero.tsx
@@ -1,7 +1,18 @@
+import { useHistory } from "react-router-dom";
+import Button from "../utils/Button";
+
 function CrearGenero() {
+
+    const history = useHistory();
+
+    const handleGuardar = () => {
+        history.push('/generos');
+    };
+
     return (
         <>
             <h3>Crear Género</h3>
+            <Button onClick={handleGuardar}>Guardar</Button>
         </>
     );
 }
--- peliculas-app/src/utils/Button.tsx
+++ peliculas-app/src/utils/Button.tsx
@@ -2,12 +2,13 @@ import { ReactElement } from "react";
 
 function Button(props: ButtonProps) {
     return (
-        <button type="button" className="btn btn-primary">{props.children}</button>
+        <button type="button" className="btn btn-primary" onClick={props.onClick}>{props.children}</button>
     );
 }
 
 interface ButtonProps {
     children: React.ReactNode;
+    onClick(): void
 }
 
 export default Button;
