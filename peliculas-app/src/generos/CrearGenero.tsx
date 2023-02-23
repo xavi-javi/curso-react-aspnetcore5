1cfcfff08af6cd0dad1d10a340df2c7f5279e91a
Javier Saucedo Rodriguez <xavi.javier.sau@gmail.com>
2023-02-23 14:56:40 -0600

    75. Desactivando el Botón Salvar

--- peliculas-app/src/generos/CrearGenero.tsx
+++ peliculas-app/src/generos/CrearGenero.tsx
@@ -18,15 +18,18 @@ function CrearGenero() {
 
             <Formik initialValues={{
                 nombre: ''
-            }} onSubmit={values => {
+            }} onSubmit={async values => {
+                await new Promise(r => setTimeout(r, 3000));
                 console.log(values);
             }} validationSchema={Yup.object({ nombre: Yup.string().required('Este campo es requerido').primeraLetraMayuscula()})}
             >
-                <Form>
-                    <FormGroupText campo="nombre" label="Nombre:" />
-                    <Button type="submit">Guardar</Button>
-                    <Link to="/generos" className="btn btn-seconday ms-3">Cancelar</Link>
-                </Form>
+                {(formikProps) => (
+                    <Form>
+                        <FormGroupText campo="nombre" label="Nombre:" />
+                        <Button type="submit" disabled={formikProps.isSubmitting} >Guardar</Button>
+                        <Link to="/generos" className="btn btn-seconday ms-3">Cancelar</Link>
+                    </Form>
+                )}
             </Formik>
         </>
     );
--- peliculas-app/src/utils/Button.tsx
+++ peliculas-app/src/utils/Button.tsx
@@ -2,18 +2,22 @@ import { ReactElement } from "react";
 
 function Button(props: ButtonProps) {
     return (
-        <button type={props.type} className="btn btn-primary" onClick={props.onClick}>{props.children}</button>
+        <button type={props.type} className="btn btn-primary" onClick={props.onClick} disabled={props.disabled}>
+            {props.children}
+        </button>
     );
 }
 
 interface ButtonProps {
     children: React.ReactNode;
-    onClick?(): void,
-    type: 'button' | 'submit'
+    onClick?(): void;
+    type: "button" | "submit";
+    disabled: boolean;
 }
 
 Button.defaultProps = {
-    type: 'button'
+    type: "button",
+    disabled: false
 };
 
 export default Button;
